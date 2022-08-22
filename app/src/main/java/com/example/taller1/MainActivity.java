b5f9493d8244cc7cfedb89d5199c872607d49b8b
Andres Parra <ca.parraa@javeriana.edu.co>
2022-08-22 16:18:45 -0500

    permissions code

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.taller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permission = findViewById(R.id.permission);
+        permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), PermissionsActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/taller1/PermissionsActivity.java
+++ app/src/main/java/com/example/taller1/PermissionsActivity.java
@@ -1,14 +1,45 @@
 package com.example.taller1;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PermissionsActivity extends AppCompatActivity {
+
+    TextView permissionStatus;
+
+    ActivityResultLauncher<String> getSinglePermission = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean result) {
+            if(result){
+                //granted
+                permissionStatus.setText("Granted");
+                permissionStatus.setTextColor(Color.GREEN);
+            }else{
+                //denied
+                permissionStatus.setText("Denied");
+                permissionStatus.setTextColor(Color.RED);
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
+        permissionStatus = findViewById(R.id.permissionStatus);
+        if(shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)){
+            Toast.makeText(this, "Permission required to show the contacts", Toast.LENGTH_LONG).show();
+        }
+        getSinglePermission.launch(Manifest.permission.READ_CONTACTS);
     }
 }
